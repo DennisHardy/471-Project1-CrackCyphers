87a7d4dd5ac8cabddc26657ef5220a7ef3a06edc
Dennis <Dennis@dhcp-10-134-220-234.uawifi.arizona.edu>
2018-02-05 12:22:01 -0700

    added permutation decrypter

--- Decrypter.m
+++ Decrypter.m
@@ -11,18 +11,18 @@ decryptedString = substitutionDecrypt(encryptedString, possiblekeys);
 
 i=0;
 correct = 'N';  
-while correct ~= 'Y' && i < 26
-    i = i+1;
-    decryptedString = shiftdecrypt(encryptedString, possiblekeys(i));
-    decryptedString = decryptedString + '@';
-    fprintf("\nPossible key: %d\n", possiblekeys(i));
-    fprintf('%s\n', decryptedString(1:100));
-    prompt = 'Does this look correct?(Y/N): ';
-    correct = input(prompt, 's');
-        if isempty(correct)
-            correct = 'N';
-        end
-end 
+% while correct ~= 'Y' && i < 26
+%     i = i+1;
+%     decryptedString = shiftdecrypt(encryptedString, possiblekeys(i));
+%     decryptedString = decryptedString + '@';
+%     fprintf("\nPossible key: %d\n", possiblekeys(i));
+     fprintf('%s\n', decryptedString(1:100));
+%     prompt = 'Does this look correct?(Y/N): ';
+%     correct = input(prompt, 's');
+%         if isempty(correct)
+%             correct = 'N';
+%         end
+% end 
 
 
 outputFile = fopen('test.txt', 'w');
---
+++ permDecrypt.m
@@ -0,0 +1,13 @@
+function decrypted = permDecrypt(encrypted,key)
+%permDecrypt decrypts a message encrypted using key columns 
+%   of a columnar transpostion
+l=1;
+encrypted = char(encrypted);
+for i = 1:key
+    for j = i:key:length(encrypted)
+        decrypted(l) = encrypted(j);
+        l=l+1;
+    end
+end
+end
+
